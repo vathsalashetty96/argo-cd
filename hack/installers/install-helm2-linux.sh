6fd278bcd56af4f60c3f453b2f22e87284423ebb
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-02-25 15:31:47 +0530

    Update install-helm2-linux.sh

--- hack/installers/install-helm2-linux.sh
+++ hack/installers/install-helm2-linux.sh
@@ -5,5 +5,7 @@ set -eux -o pipefail
 
 [ -e $DOWNLOADS/helm2.tar.gz ] || curl -sLf --retry 3 -o $DOWNLOADS/helm2.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v${helm2_version}-linux-$ARCHITECTURE.tar.gz
 mkdir -p /tmp/helm2 && tar -C /tmp/helm2 -xf $DOWNLOADS/helm2.tar.gz
+
+tar -xvzf helm2.tar.gz
 cp /tmp/helm2/linux-$ARCHITECTURE/helm $BIN/helm2
 helm2 version --client
