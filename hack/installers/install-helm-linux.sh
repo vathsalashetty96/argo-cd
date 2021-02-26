2df12007246249c90610cf56f093b98f2c2adb63
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-02-26 15:16:09 +0530

    Update install-helm-linux.sh

--- hack/installers/install-helm-linux.sh
+++ hack/installers/install-helm-linux.sh
@@ -6,4 +6,4 @@ set -eux -o pipefail
 [ -e $DOWNLOADS/helm.tar.gz ] || curl -sLf --retry 3 -o $DOWNLOADS/helm.tar.gz https://get.helm.sh/helm-v${helm3_version}-linux-$ARCHITECTURE.tar.gz
 mkdir -p /tmp/helm && tar -C /tmp/helm -xf $DOWNLOADS/helm.tar.gz
 cp /tmp/helm/linux-$ARCHITECTURE/helm $BIN/helm
-helm version --client
+
