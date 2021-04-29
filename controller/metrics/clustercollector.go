77cc2fa9e08577646860bdd54be2f600c08ff830
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-04-29 15:35:59 +0530

    Update clustercollector.go

--- controller/metrics/clustercollector.go
+++ controller/metrics/clustercollector.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/argoproj/gitops-engine/pkg/cache"
+	"github.com/vathsalashetty96/gitops-engine/pkg/cache"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
