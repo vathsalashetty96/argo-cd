fc1e2c313bdfddb849b68780d838489098e02312
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-04-29 15:29:59 +0530

    Update cluster_test.go

--- cmd/util/cluster_test.go
+++ cmd/util/cluster_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/rest"
 
-	"github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
+	"github.com/vathsalashetty96/argo-cd/pkg/apis/application/v1alpha1"
 )
 
 func Test_newCluster(t *testing.T) {
