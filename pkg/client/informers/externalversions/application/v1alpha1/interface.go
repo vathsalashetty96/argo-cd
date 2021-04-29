f8e057872bf08cb515976e49c98eafbc0e8c2bc5
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-04-29 18:58:21 +0530

    Update interface.go

--- pkg/client/informers/externalversions/application/v1alpha1/interface.go
+++ pkg/client/informers/externalversions/application/v1alpha1/interface.go
@@ -3,7 +3,7 @@
 package v1alpha1
 
 import (
-	internalinterfaces "github.com/argoproj/argo-cd/pkg/client/informers/externalversions/internalinterfaces"
+	internalinterfaces "github.com/vathsalashetty96/argo-cd/pkg/client/informers/externalversions/internalinterfaces"
 )
 
 // Interface provides access to all the informers in this group version.
