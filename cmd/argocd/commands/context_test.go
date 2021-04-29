15cc91e2f925edaa7f4d9d5e083995d13e492cb6
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-04-29 14:06:27 +0530

    Update context_test.go

--- cmd/argocd/commands/context_test.go
+++ cmd/argocd/commands/context_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/argoproj/argo-cd/util/localconfig"
+	"github.com/vathsalashetty96/argo-cd/util/localconfig"
 )
 
 const testConfig = `contexts:
