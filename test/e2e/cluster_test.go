43b0f03afca7288be03bdd07dbe603cdaafb3c8d
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-04-30 12:18:59 +0530

    Update cluster_test.go

--- test/e2e/cluster_test.go
+++ test/e2e/cluster_test.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	. "github.com/argoproj/argo-cd/test/e2e/fixture"
-	. "github.com/argoproj/argo-cd/util/errors"
+	. "github.com/vathsalashetty96/argo-cd/test/e2e/fixture"
+	. "github.com/vathsalashetty96/argo-cd/util/errors"
 )
 
 func TestClusterList(t *testing.T) {
