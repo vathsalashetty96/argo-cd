8aa26b37e769626beaead1ae407b4a64911cebe3
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-05-01 18:57:29 +0530

    Update repository_test.go

--- util/db/repository_test.go
+++ util/db/repository_test.go
@@ -3,17 +3,17 @@ package db
 import (
 	"testing"
 
-	"github.com/argoproj/argo-cd/util/settings"
+	"github.com/vathsalashetty96/argo-cd/util/settings"
 )
 
 func TestRepoURLToSecretName(t *testing.T) {
 	tables := map[string]string{
-		"git://git@github.com:argoproj/ARGO-cd.git": "repo-83273445",
-		"https://github.com/argoproj/ARGO-cd":       "repo-1890113693",
-		"https://github.com/argoproj/argo-cd":       "repo-42374749",
-		"https://github.com/argoproj/argo-cd.git":   "repo-821842295",
-		"https://github.com/argoproj/argo_cd.git":   "repo-1049844989",
-		"ssh://git@github.com/argoproj/argo-cd.git": "repo-3569564120",
+		"git://git@github.com:vathsalashetty96/ARGO-cd.git": "repo-83273445",
+		"https://github.com/vathsalashetty96/ARGO-cd":       "repo-1890113693",
+		"https://github.com/vathsalashetty96/argo-cd":       "repo-42374749",
+		"https://github.com/vathsalashetty96/argo-cd.git":   "repo-821842295",
+		"https://github.com/vathsalashetty96/argo_cd.git":   "repo-1049844989",
+		"ssh://git@github.com/vathsalashetty96/argo-cd.git": "repo-3569564120",
 	}
 
 	for k, v := range tables {
@@ -25,10 +25,10 @@ func TestRepoURLToSecretName(t *testing.T) {
 
 func Test_CredsURLToSecretName(t *testing.T) {
 	tables := map[string]string{
-		"git://git@github.com:argoproj":  "creds-2483499391",
-		"git://git@github.com:argoproj/": "creds-1465032944",
-		"git@github.com:argoproj":        "creds-2666065091",
-		"git@github.com:argoproj/":       "creds-346879876",
+		"git://git@github.com:vathsalashetty96":  "creds-2483499391",
+		"git://git@github.com:vathsalashetty96/": "creds-1465032944",
+		"git@github.com:vathsalashetty96":        "creds-2666065091",
+		"git@github.com:vathsalashetty96/":       "creds-346879876",
 	}
 
 	for k, v := range tables {
