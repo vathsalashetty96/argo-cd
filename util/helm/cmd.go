ccf575fa254aaee2d56de53b941013e2e68e1afa
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-05-01 19:43:04 +0530

    Update cmd.go

--- util/helm/cmd.go
+++ util/helm/cmd.go
@@ -9,8 +9,8 @@ import (
 	"path/filepath"
 	"regexp"
 
-	executil "github.com/argoproj/argo-cd/util/exec"
-	"github.com/argoproj/argo-cd/util/io"
+	executil "github.com/vathsalashetty96/argo-cd/util/exec"
+	"github.com/vathsalashetty96/argo-cd/util/io"
 )
 
 // A thin wrapper around the "helm" command, adding logging and error translation.
