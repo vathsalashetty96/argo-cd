591601cc72fe1263d4d969d622a1d464d33afb62
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-05-02 15:48:05 +0530

    Update cache.go

--- util/cache/appstate/cache.go
+++ util/cache/appstate/cache.go
@@ -8,8 +8,8 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/spf13/cobra"
 
-	appv1 "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
-	cacheutil "github.com/argoproj/argo-cd/util/cache"
+	appv1 "github.com/vathsalashetty96/argo-cd/pkg/apis/application/v1alpha1"
+	cacheutil "github.com/vathsalashetty96/argo-cd/util/cache"
 )
 
 var ErrCacheMiss = cacheutil.ErrCacheMiss
