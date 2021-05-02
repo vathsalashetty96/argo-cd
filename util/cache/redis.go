28d9f605f99a8a488c09dce73992d25280d6c3a9
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-05-02 15:51:21 +0530

    Update redis.go

--- util/cache/redis.go
+++ util/cache/redis.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"time"
 
-	ioutil "github.com/argoproj/argo-cd/util/io"
+	ioutil "github.com/vathsalashetty96/argo-cd/util/io"
 
 	rediscache "github.com/go-redis/cache/v8"
 	"github.com/go-redis/redis/v8"
