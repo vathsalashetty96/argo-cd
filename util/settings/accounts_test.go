518ea4b80dc7eb6f309a726a34cc77cf8058d094
vathsalashetty96 <82566301+vathsalashetty96@users.noreply.github.com>
2021-05-02 11:07:54 +0530

    Update accounts_test.go

--- util/settings/accounts_test.go
+++ util/settings/accounts_test.go
@@ -11,7 +11,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/argoproj/argo-cd/common"
+	"github.com/vathsalashetty96/argo-cd/common"
 )
 
 func TestGetAccounts_NoAccountsConfigured(t *testing.T) {
