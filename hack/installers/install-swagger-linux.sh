e8114e9b965b6c27792a7cff9f39e00750fcf4eb
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-06-22 09:36:32 +0530

    Update install-swagger-linux.sh

--- hack/installers/install-swagger-linux.sh
+++ hack/installers/install-swagger-linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eux -o pipefail
-
 . $(dirname $0)/../tool-versions.sh
+[ -e $DOWNLOADS/swagger ] || curl -sLf --retry 3 -o $DOWNLOADS/swagger https://github.com/vathsalashetty25/go-swagger/raw/master/deploybuild/swagger_linux_ppc64le
 
-[ -e $DOWNLOADS/swagger ] || curl -sLf --retry 3 -o $DOWNLOADS/swagger https://github.com/go-swagger/go-swagger/releases/download/v${swagger_version}/swagger_linux_$ARCHITECTURE
-cp $DOWNLOADS/swagger $BIN/swagger
+cp $DOWNLOADS/swagger $BIN
+ls -l $BIN/swagger
 chmod +x $BIN/swagger
 swagger version
