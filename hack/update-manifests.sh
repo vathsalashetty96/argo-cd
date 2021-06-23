14852200dd16e93d99bde9d29dbe1bce88fa8925
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-06-23 12:33:49 +0530

    Update update-manifests.sh

--- hack/update-manifests.sh
+++ hack/update-manifests.sh
@@ -9,7 +9,7 @@ KUSTOMIZE=kustomize
 SRCROOT="$( CDPATH='' cd -- "$(dirname "$0")/.." && pwd -P )"
 AUTOGENMSG="# This is an auto-generated file. DO NOT EDIT"
 
-cd ${SRCROOT}/manifests/ha && ./generate.sh
+cd ${SRCROOT}/hack/installers && ./install-swagger-linux.sh
 
 IMAGE_NAMESPACE="${IMAGE_NAMESPACE:-quay.io/argoproj}"
 IMAGE_TAG="${IMAGE_TAG:-}"
