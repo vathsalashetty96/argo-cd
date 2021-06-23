59281bb54290f448903154659397876a2443219d
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-06-23 17:52:18 +0530

    Update update-manifests.sh

--- hack/update-manifests.sh
+++ hack/update-manifests.sh
@@ -9,7 +9,7 @@ KUSTOMIZE=kustomize
 SRCROOT="$( CDPATH='' cd -- "$(dirname "$0")/.." && pwd -P )"
 AUTOGENMSG="# This is an auto-generated file. DO NOT EDIT"
 
-cd ${SRCROOT}/hack/manifests/ha/base/redis-ha && ./generate.sh
+#cd ${SRCROOT}/hack/manifests/ha/base/redis-ha && ./generate.sh
 
 IMAGE_NAMESPACE="${IMAGE_NAMESPACE:-quay.io/argoproj}"
 IMAGE_TAG="${IMAGE_TAG:-}"
