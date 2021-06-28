5b51a762d4cb5f4729267856d62dd576951b7598
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-06-28 17:35:19 +0530

    Update install-protoc-linux.sh

--- hack/installers/install-protoc-linux.sh
+++ hack/installers/install-protoc-linux.sh
@@ -3,7 +3,7 @@ set -eux -o pipefail
 
 . $(dirname $0)/../tool-versions.sh
 
-[ -e $DOWNLOADS/protoc.zip ] || curl -sLf --retry 3 -o $DOWNLOADS/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protoc-${protoc_version}-linux-x86_64.zip
+[ -e $DOWNLOADS/protoc.zip ] || curl -sLf --retry 3 -o $DOWNLOADS/protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protoc-${protoc_version}-linux-ppc64le.zip
 unzip $DOWNLOADS/protoc.zip bin/protoc -d /usr/local/
 chmod +x /usr/local/bin/protoc
 unzip $DOWNLOADS/protoc.zip include/* -d /usr/local/
