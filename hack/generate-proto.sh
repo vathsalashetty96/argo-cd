52cc12df56599f3fe33af75a5a12d0159f147b35
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-06-28 19:27:43 +0530

    Update generate-proto.sh

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -47,7 +47,7 @@ APIMACHINERY_PKGS=(
 
 ${PROJECT_ROOT}/dist/go-to-protobuf \
     --go-header-file=${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
-    --packages=$(IFS=, ; echo "${PACKAGES[*]}") \
+    
     --apimachinery-packages=$(IFS=, ; echo "${APIMACHINERY_PKGS[*]}") \
     --proto-import=./vendor
 
