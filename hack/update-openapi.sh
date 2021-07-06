59e2db995ba12a45f04642674332a8bcf0720661
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-07-06 10:41:09 +0530

    Update update-openapi.sh

--- hack/update-openapi.sh
+++ hack/update-openapi.sh
@@ -17,6 +17,6 @@ go build -o dist/openapi-gen ${CODEGEN_PKG}/cmd/openapi-gen
   --report-filename pkg/apis/api-rules/violation_exceptions.list \
   $@
 
-go build -o ./dist/gen-crd-spec ${PROJECT_ROOT}/hack/gen-crd-spec
+go build -o ./dist/gen-crd-spec 
 ./dist/gen-crd-spec
 
